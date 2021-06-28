494c9d912e448e0ec702e15ff9db675032a8fd43
deoliaugusto <deoliaugusto@gmail.com>
2021-06-27 23:23:46 -0300

    Test for component Header

--- src/components/Header/Header.spec.jsx
+++ src/components/Header/Header.spec.jsx
@@ -1,5 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import Header from '.';
+import '@testing-library/jest-dom/extend-expect';
+
 describe('<Header />', () => {
-  it('is a dummy test', () => {
-    expect(1).toBe(1);
+  it(`should render the heading with provided title`, () => {
+    const { debug } = render(<Header />);
+    debug();
+    const title = 'Github';
+    const header = screen.getByRole('heading', { name: /github/i });
+    expect(header).toHaveTextContent(`${title}`);
+  });
+
+  it('should render a small with provided subtitle', () => {
+    render(<Header />);
+    const subtitle = 'buscar por usuário';
+    const small = screen.getByText(/buscar por usuário/i);
+    expect(small).toHaveTextContent(subtitle);
   });
 });
